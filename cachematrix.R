a2d8d9b34bb68d4a4d9a0681e0ed5b7f7e29c188
Igor <igormarkaida@users.noreply.github.com>
2015-05-24 13:52:45 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## The first function, makeVector creates a special "vector",
+## which is really a list containing a function to:
+## set the value of the vector
+## get the value of the vector
+## set the value of the mean
+## get the value of the mean
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() {
+                x
+        }
+        setmean <- function(mean) {
+                m <<- mean      
+        } 
+        getmean <- function(){
+                m
+        }
+        list(set = set, get = get, setmean = setmean, getmean = getmean)
 }
 
+## The following function calculates the mean of the
+## special "vector" created with the above function.
+## However, it first checks to see if the mean has 
+## already been calculated. If so, it gets the mean
+## from the cache and skips the computation. 
+## Otherwise, it calculates the mean of the data
+## and sets the value of the mean in the cache via
+## the setmean function.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {                       # if the mean was cached
+                message("getting cached data")
+                return(m)                       # exit program without running subsequent code
+        }
+        data <- x$get()                         # otherwise, put data in "data" 
+        m <- mean(data, ...)                    # compute the mean of the data 
+        x$setmean(m)                            # call function to cache the mean
+        m                                       # return the mean 
 }
